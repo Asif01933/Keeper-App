3006febaae7507d2c40f013d3a973d349a581d57
Asif <asifhayatanjon@gmail.com>
2022-09-17 14:19:23 +0600

    init

---
+++ build/precache-manifest.cd15d84ce4d78b0df23fc21da7189990.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04495a1965f5d61f003a0ddad29af097",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50fed50bc5636fd62729",
+    "url": "/static/js/2.f16a517a.chunk.js"
+  },
+  {
+    "revision": "79bc1dcc079f3de0715b",
+    "url": "/static/js/main.10291b9e.chunk.js"
+  },
+  {
+    "revision": "325f4b8f32369198a2c5",
+    "url": "/static/js/runtime-main.4e550677.js"
+  }
+]);
